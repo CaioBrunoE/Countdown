f5f314185a675ba4a0d80bd1a506241d88844717
Caio Bruno <caiobruno90@yahoo.com>
2024-01-02 13:42:05 -0300

    Criando logica de contagem regressiva

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import Counter from './components/Counter'
 
 import NewYear from "./assets/newyear.jpg"
 
+import useCountDown from './hooks/useContdown'
+
 import './App.css'
 
+
 function App() {
+  
+  const  [day , hour , minute , second]= useCountDown("Jan 1, 2025 00:00:00");
+
   return (
  <div className='app' style={{backgroundImage:`url(${NewYear})`}}>
        <div className="container">
        <Title title="Contagen regressiva para 2025"/>
         <div className="countdown-container">
-          <Counter title="Dias" number={2}/>
-          <Counter title="Hora" number={2}/>
-          <Counter title="Minutos" number={2}/>
-          <Counter title="Segundos" number={2}/>
+          <Counter title="Dias" number={day}/>
+          <Counter title="Hora" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundos" number={second}/>
           </div>  
        </div>
  </div>
