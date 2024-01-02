64a5d2b3fe4667fa0815ad7cc4fe5de654df3602
Caio Bruno <caiobruno90@yahoo.com>
2024-01-02 11:46:30 -0300

    Criando componetes principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+
+import NewYear from "./assets/newyear.jpg"
 
 import './App.css'
 
@@ -5,8 +9,16 @@ function App() {
  
 
   return (
- <div className='app'>
-       <h1>Countdown</h1>
+ <div className='app' style={{backgroundImage:`url(${NewYear})`}}>
+       <div className="container">
+       <Title title="Contagen regressiva para 2025"/>
+        <div className="countdown-container">
+          <Counter title="Dias" number={2}/>
+          <Counter title="Hora" number={2}/>
+          <Counter title="Minutos" number={2}/>
+          <Counter title="Segundos" number={2}/>
+          </div>  
+       </div>
  </div>
   )
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+
+function Title({title}) {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+export default Title
